4081105e810a8786f7da4cd4648d39118a65b9bc
Abdevlo <abdallahmuath0@gmail.com>
2023-05-24 18:01:27 +0530

    DBContextClass

---
+++ HLCV.App/DBContextClass.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HLCV.App
+{
+    public class DBContextClass : DbContext
+    {
+        public DBContextClass(DbContextOptions<DBContextClass> contextOptions) : base(contextOptions)
+        {
+        }
+    }
+}
--- HLCV.App/Program.cs
+++ HLCV.App/Program.cs
@@ -1,17 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
+using HLCV.App;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+builder.Services.AddDbContext<DBContextClass>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("HLCVDB"));
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
