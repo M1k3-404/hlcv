6736a126bec679b3a9f00ee6f851cfd93d0eea66
M1k3-404 <muthula.alwis@gmail.com>
2023-05-25 19:22:34 +0530

    Front End Final

---
+++ HLCV.App/ClientApp/src/data/bookingsData.js
@@ -0,0 +1,33 @@
+export const BookingsData = [
+    {
+        'BookingCode': '1djhf2893',
+        'FirstName': 'Roshain',
+        'LastName': 'Fernando',
+        'NIC': '200031481024',
+        'Email': 'roshain.fernando@gmail.com',
+        'MobileNumber': '0712345678',
+        'NumberOfPeople': 3,
+        'Arrival': '02-04-2023',
+        'Departure': '04-04-2023',
+        'TotalAmount': 32000,
+        'SDR': 0,
+        'STR': 1,
+        'DRB': 0
+    },
+
+    {
+        'BookingCode': '1d2942cw3',
+        'FirstName': 'Tom',
+        'LastName': 'Doe',
+        'NIC': '221331452024',
+        'Email': 'Tom.Doe@gmail.com',
+        'MobileNumber': '0712245678',
+        'NumberOfPeople': 2,
+        'Arrival': '04-04-2023',
+        'Departure': '05-04-2023',
+        'TotalAmount': 22000,
+        'SDR': 2,
+        'STR': 0,
+        'DRB': 0
+    }
+]
